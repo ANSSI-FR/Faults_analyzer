3fba45379033e2868316784a385357a9470c108a
trouchkine <thomas.trouchkine@ssi.gouv.fr>
2020-01-28 15:04:02 +0100

    Updating list parsing

--- modules/dict_editor.py
+++ modules/dict_editor.py
@@ -24,17 +24,7 @@ def floatable(v):
     except:
         return False
 
-def listable(s):
-    if (s[0] == "[") and (s[-1] == "]"):
-        return True
-    else:
-        return False
-
 def convert_str(s):
-    if intable(s):
-        return int(s)
-    if floatable(s):
-        return float(s)
     if listable(s):
         s = s[1:-1].split(",")
         return [e.replace("\,",",") for e in s]
@@ -66,12 +56,9 @@ class DictEditor(Cmd):
                 print("Error: key not in dictionary.")
 
     def do_add(self, inp):
-        inp = inp.rstrip().split(" ")
-        if len(inp) != 2:
-            print("Error: missing key or value.")
-        else:
-            val = convert_str(inp[1])
-            self.d.update({inp[0]: val})
+        val = self.parse_inp(inp)
+        if type(val) != None:
+            self.d.update({val[0]: val[1]})
 
     def do_typeof(self, inp):
         inp = inp.rstrip().split(" ")
@@ -79,6 +66,26 @@ class DictEditor(Cmd):
             print("Error: no key given.")
         else:
             print(type(self.d[inp[0]]))
+
+    def parse_inp(self, inp):
+        inp = inp.split(" ", 1)
+        if len(inp) != 2:
+            print("Error: missing key or value")
+            return None
+        val = inp[1]
+        if (val[0] == '"') and (val[-1] == '"'):
+            val = val[1:-1]
+        if (val[0] == '[') and (val[-1] == ']'):
+            val = val[1:-1]
+            val = val.replace("\,", "$$$virg$$$")
+            val = val.replace(", ", ",")
+            val = val.split(",")
+            val = [e.replace("$$$virg$$$",",") for e in val]
+        elif intable(val):
+            val = int(val)
+        elif floatable(val):
+            val = float(val)
+        return (inp[0], val)
 
 if __name__ == "__main__":
     d = {
