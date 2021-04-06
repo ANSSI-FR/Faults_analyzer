5e7e8f033cf388451ed138f2427d97a21bf28231
Thomas Trouchkine <thomas@trouchkine.com>
2021-04-06 16:33:34 +0200

    Adding url to docs-src/_config.yml

--- docs/assets/js/just-the-docs.js
+++ docs/assets/js/just-the-docs.js
@@ -53,7 +53,7 @@ function initNav() {
 
 function initSearch() {
   var request = new XMLHttpRequest();
-  request.open('GET', '/Fault_analyzer/assets/js/search-data.json', true);
+  request.open('GET', 'https://anssi-fr.github.io/Fault_analyzer/assets/js/search-data.json', true);
 
   request.onload = function(){
     if (request.status >= 200 && request.status < 400) {
@@ -430,7 +430,7 @@ jtd.getTheme = function() {
 
 jtd.setTheme = function(theme) {
   var cssFile = document.querySelector('[rel="stylesheet"]');
-  cssFile.setAttribute('href', '/Fault_analyzer/assets/css/just-the-docs-' + theme + '.css');
+  cssFile.setAttribute('href', 'https://anssi-fr.github.io/Fault_analyzer/assets/css/just-the-docs-' + theme + '.css');
 }
 
 // Document ready
